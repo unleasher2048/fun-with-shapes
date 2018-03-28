9ff0064d39f38cfb6944cb43a371d78b58163449
Andrzej Jaromin <andrzejjaromin@gmail.com>
2018-03-28 09:13:13 +0200

    Initial commit - empty project

---
+++ src/main/java/com/caleb/example/funwithshapes/FunWithShapesApplication.java
@@ -0,0 +1,12 @@
+package com.caleb.example.funwithshapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithShapesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunWithShapesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/caleb/example/funwithshapes/FunWithShapesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.caleb.example.funwithshapes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FunWithShapesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
